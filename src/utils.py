0ab476ff661cf7fdf02273c90e1dea789589d829
radhikasethi2011 <radhika.sethi0312@gmail.com>
2020-06-04 17:41:01 +0530

    made changes

--- src/utils.py
+++ src/utils.py
@@ -1,10 +1,9 @@
-def space(para, n):
+def split_paragraph(para, n):
     """Returns a string that's sliced after n words.
 
       Input -> string, n->after n words, adding a \n.
   """
     res = para.split()
-    ans = []
     ans = [" ".join(res[i : i + n]) for i in range(0, len(res), n)]
-    ans = "\n".join(ans)
-    return ans
+    return "\n".join(ans)
+  
