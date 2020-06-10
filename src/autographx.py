2d9559a1e0e9400b4e2633137c480c95d71825f3
meghanabhange <meghana.bhange@cumminscollege.in>
2020-06-11 03:36:24 +0530

    Clean code

--- src/autographx.py
+++ src/autographx.py
@@ -1,70 +1,27 @@
 import datetime
-import numpy as np
-from matplotlib.backends.backend_pdf import PdfPages
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
 from pathlib import Path
 
-autographs = {
-    "Meghana": "Heya, this is my autograph for you",
-    "Shriya": "This is another test autograph\n I hope you like it",
-}
+import matplotlib.image as mpimg
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.backends.backend_pdf import PdfPages
+
+from utils import (add_image, add_quote, get_autos, get_display_img,
+                   split_paragraph)
 
 
-def get_autographs(pathtofile):
-    """Takes input to the yearbook dir and return a dictionary of all the autographs
-
-    Arguments:
-        pathtofile {[type]} -- path to the yearbook dir
-
-    Returns:
-        [dict] -- {name : {nameofautographer : autograph}
-    """
-
-    autos = {}
-
-    path = Path(pathtofile)
-    assert path.is_dir()
-    file_list = []
-    for x in path.iterdir():
-        if x.is_dir():
-            file_list.append(x)
-    # print(f"Found files {len(file_list)} -- {file_list}")
-
-    for f in file_list:
-        name = str(f)[len(pathtofile) + 1 :]
-        autos[name] = {}
-        for x in f.iterdir():
-            if not (str(x) == f"{pathtofile}/{name}/{name}.txt") and not (
-                str(x) == f"{pathtofile}/{name}/{name}.jpg"
-                or str(x) == f"{pathtofile}/{name}/{name}.png"
-            ):
-                try:
-                    l = len(pathtofile) + len(name) + 12
-                    f = open(x, "r").read().replace("\n", " ").split()
-                    s = []
-                    for i in range(0, len(f), 10):
-                        s.append(" ".join(f[i : i + 10]))
-                    output = "\n".join(s)
-                except:
-                    output = "Input Error"
-                autos[name][str(x)[l:-4]] = output
-
-    return autos
-
-
-def autographs_topdf(autos: dict, pathtofile) -> None:
+def autographs_topdf(autos: list, pathtofile) -> None:
     """Takes autographs and converts them to pdfs
 
     Arguments:
         autographs {dict} -- Dict of name-autograph mapping
         name {str} -- name of the person who these autographs are for
     """
-    for name, autographs in autos.items():
-        with PdfPages(f"{name}.pdf") as pdf:
+    for person in autos:
+        with PdfPages(f"{person['Name']}.pdf") as pdf:
             print_cover(pdf)
             print_grid(autos, pdf, pathtofile)
-            print_autographs(autographs, pdf)
+            print_autographs(person["autographs"], pdf)
 
 
 def print_cover(pdf):
@@ -78,113 +35,33 @@ def print_cover(pdf):
     plt.close()
 
 
-def get_display_img(sname, pathtofile):
-    if Path(f"{pathtofile}/{sname}/{sname}.jpg").is_file():
-        return f"{pathtofile}/{sname}/{sname}.jpg"
-    elif Path(f"{pathtofile}/{sname}/{sname}.png").is_file():
-        return f"{pathtofile}/{sname}/{sname}.png"
-    else:
-        # print(f"they don't match for {sname}")
-        return "unknown.png"
-
-
 def print_grid(autos, pdf, pathtofile):
-    all_name = list(autos.keys())
-    for sname in range(0, len(all_name), 2):
+    for sno in range(0, len(autos), 4):
         plt.figure(figsize=(10.8, 19.2))
-        plt.subplot(4, 1, 1)
-        try:
-            img = mpimg.imread(get_display_img(all_name[sname], pathtofile))
-            plt.imshow(img)
-        except:
-            img = mpimg.imread("unknown.png")
-            plt.imshow(img)
-        plt.axis("off")
+        plt.subplot(4, 2, 1)
+        add_image(autos, plt, sno)
 
-        plt.subplot(4, 1, 2)
-        try:
-            value = (
-                open(f"{pathtofile}/{all_name[sname]}/{all_name[sname]}.txt")
-                .readlines()[1]
-                .split()
-            )
-            s = []
-            for i in range(0, len(value), 5):
-                s.append(" ".join(value[i : i + 5]))
-            output = "\n".join(s)
-        except:
-            output = "Wrong formatting"
-        plt.text(
-            0.5,
-            0.5,
-            output,
-            horizontalalignment="center",
-            verticalalignment="center",
-            fontdict={
-                "family": "serif",
-                # "color": "#f0bc81",
-                "weight": "normal",
-                "size": 20,
-            },
-        )
+        plt.subplot(4, 2, 3)
+        add_quote(autos, plt, sno)
 
-        plt.title(
-            f"{all_name[sname]}",
-            fontdict={
-                "family": "serif",
-                # "color": "#f0bc81",
-                "weight": "normal",
-                "size": 24,
-            },
-        )
-        plt.axis("off")
+        plt.subplot(4, 2, 2)
+        add_image(autos, plt, sno + 1)
 
-        plt.subplot(4, 1, 3)
-        try:
-            img = mpimg.imread(get_display_img(all_name[sname + 1], pathtofile))
-            plt.imshow(img)
-        except:
-            img = mpimg.imread("unknown.png")
-            plt.imshow(img)
-        plt.axis("off")
+        plt.subplot(4, 2, 4)
+        add_quote(autos, plt, sno + 1)
 
-        plt.subplot(4, 1, 4)
-        try:
-            value = (
-                open(f"{pathtofile}/{all_name[sname+1]}/{all_name[sname+1]}.txt")
-                .readlines()[1]
-                .split()
-            )
-            s = []
-            for i in range(0, len(value), 5):
-                s.append(" ".join(value[i : i + 5]))
-            output = "\n".join(s)
-        except:
-            output = "Wrong formatting"
-        plt.text(
-            0.5,
-            0.5,
-            output,
-            horizontalalignment="center",
-            verticalalignment="center",
-            fontdict={
-                "family": "serif",
-                # "color": "#f0bc81",
-                "weight": "normal",
-                "size": 20,
-            },
-        )
+        plt.subplot(4, 2, 5)
+        add_image(autos, plt, sno + 2)
 
-        plt.title(
-            f"{all_name[sname+1]}",
-            fontdict={
-                "family": "serif",
-                # "color": "#f0bc81",
-                "weight": "normal",
-                "size": 24,
-            },
-        )
-        plt.axis("off")
+        plt.subplot(4, 2, 7)
+        add_quote(autos, plt, sno + 2)
+
+        plt.subplot(4, 2, 6)
+        add_image(autos, plt, sno + 3)
+
+        plt.subplot(4, 2, 8)
+        add_quote(autos, plt, sno + 3)
+
         pdf.savefig()
         plt.close()
 
@@ -227,6 +104,6 @@ def print_autographs(autographs, pdf):
         plt.close()
 
 
-autos = get_autographs("YearbookENTC")
+autos = get_autos("YearbookENTC")
 
 autographs_topdf(autos, "YearbookENTC")
---
+++ src/utils.py
@@ -0,0 +1,135 @@
+from pathlib import Path
+
+import gdown
+import matplotlib.image as mpimg
+import matplotlib.pyplot as plt
+import pandas as pd
+from matplotlib.backends.backend_pdf import PdfPages
+
+
+def split_paragraph(para, n):
+    """Returns a string that's sliced after n words.
+    """
+    res = para.split()
+    ans = [" ".join(res[i : i + n]) for i in range(0, len(res), n)]
+    return "\n".join(ans)
+
+
+def get_files_from_gdrive(url: str, fname: str) -> None:
+    file_id = url.split("/")[3].split("?")[1]
+    print(file_id)
+    url = f"https://drive.google.com/uc?{file_id}"
+    gdown.download(url, fname, quiet=False)
+
+
+def get_autos(df, filepath="YearbookENTC"):
+    df = pd.read_csv("YearbookENTC/details.csv")
+    df["query_name"] = df["First Name"] + df["Last Name"]
+    df["query_name"] = df["query_name"].apply(lambda x: x.lower())
+    df.set_index("query_name", inplace=True)
+
+    autos = []
+    filepath = Path(filepath)
+    assert filepath.is_dir()
+    file_list = []
+    for x in filepath.iterdir():
+        if x.is_dir():
+            file_list.append(x)
+    print(file_list)
+
+    for f in file_list:
+        details = {}
+        name = str(f)[len(str(filepath)) + 1 :]
+        not_filled = []
+        if name in list(df.index):
+            details["Name"] = (
+                df.loc[name]["First Name"] + " " + df.loc[name]["Last Name"]
+            )
+            details["Quote"] = df.loc[name]["Quote for yearbook"]
+            # get_files_from_gdrive(
+            #     df.loc[name]["Year Book Image"],
+            #     f"imgdata/{df.loc[name]['filename of your image (With extension .jpg or .png)']}",
+            # )
+            details["Image"] = (
+                f"imgdata/{df.loc[name]['filename of your image (With extension .jpg or .png)']}",
+            )
+            details["autographs"] = {}
+        else:
+            print(f"Something is wrong with {name}")
+            continue
+        for x in f.iterdir():
+            if not (str(x) == f"{str(filepath)}/{name}/{name}.txt") and not (
+                str(x) == f"{str(filepath)}/{name}/{name}.jpg"
+                or str(x) == f"{str(filepath)}/{name}/{name}.png"
+            ):
+
+                try:
+                    l = len(str(filepath)) + len(name) + 12
+                    f = open(x, "r").read().replace("\n", " ").replace("\ufeff", "")
+                    output = split_paragraph(f, 10)
+                except:
+                    output = "Input Error"
+                try:
+                    pname = (
+                        df.loc[str(x)[l:-4]]["First Name"]
+                        + " "
+                        + df.loc[str(x)[l:-4]]["Last Name"]
+                    )
+                except:
+                    pname = str(x)[l:-4]
+                details["autographs"][pname] = output
+        autos.append(details)
+    return autos
+
+
+def get_display_img(imgpath):
+    if Path(imgpath).is_file():
+        return str(imgpath)
+    else:
+        return "unknown.png"
+
+
+def add_image(autos, plt, sno):
+    try:
+        imgpath = autos[sno]["Image"][0]
+        img = mpimg.imread(get_display_img(imgpath))
+        plt.imshow(img)
+    except:
+        img = mpimg.imread("unknown.png")
+        plt.imshow(img)
+    plt.axis("off")
+
+
+def add_quote(autos, plt, sno):
+    try:
+        output = autos[sno]["Quote"]
+        output = split_paragraph(output, 4)
+    except:
+        output = "Wrong formatting for quote"
+    plt.text(
+        0.5,
+        0.5,
+        output,
+        horizontalalignment="center",
+        verticalalignment="center",
+        fontdict={
+            "family": "serif",
+            # "color": "#f0bc81",
+            "weight": "normal",
+            "size": 20,
+        },
+    )
+    try:
+        title = autos[sno]["Name"]
+    except:
+        title = "error"
+    plt.title(
+        title,
+        fontdict={
+            "family": "serif",
+            # "color": "#f0bc81",
+            "weight": "normal",
+            "size": 24,
+        },
+    )
+    plt.axis("off")
