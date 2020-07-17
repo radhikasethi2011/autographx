c423e8193c2597a479335b18815898bbf461002d
radhikasethi2011 <radhika.sethi0312@gmail.com>
2020-07-17 16:08:07 +0530

    adding url passing

---
+++ index.js
@@ -0,0 +1,11 @@
+
+function console(){
+  myvar =
+  alert ({autos[i]["Name"]});
+
+  var parsed= JSON.parse(myvar);
+  document.getElementById(parsed[0]['autographs']);
+
+    }
+
+
--- src/flaskapp.py
+++ src/flaskapp.py
@@ -1,7 +1,13 @@
 from utils import get_autos, split_paragraph, get_display_img
+from flask import request
 
 # import autographx
 from flask import Flask, render_template
+
+
+#for x,y in autos[0]['autographs'].items():  to print name : autograph 
+#...     print(x, " : ", y)
+
 
 autos = get_autos("YearbookENTC")
 app = Flask(__name__)
@@ -11,9 +17,11 @@ app = Flask(__name__)
 def homepage():
     return render_template("index.html", len=len(autos), autos=autos)
 
-@app.route("/source/<name>")
+@app.route("/autographs/<string:name>")
 def index_func(name):
-    return render_template("whiteauto.html", len=len(autos), autos=autos, name=name) 
+    my_var = request.args.get('my_var',None)
+    return """<h1> the name is : {}</h1>""".format(my_var)
+    #return render_template("whiteauto.html", len=len(autos), autos=autos, my_var=my_var) 
 
 if __name__ == "__main__":
     app.run(use_reloader=True, debug=True)
