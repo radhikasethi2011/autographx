0d50e66c61a88a777e5cef7f49bc96b456037e3a
radhikasethi2011 <radhika.sethi0312@gmail.com>
2020-07-18 10:17:10 +0530

    mend

--- src/utils.py
+++ src/utils.py
@@ -142,7 +142,5 @@ def split_paragraph(para, n):
     res = para.split()
     ans = [" ".join(res[i : i + n]) for i in range(0, len(res), n)]
     return "\n".join(ans)
-
-
 # autos = get_autos("YearbookENTC")
 # print(autos)
