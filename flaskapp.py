4c95f48751f75179b94d9a6b0d24a134a62f7804
radhikasethi2011 <radhika.sethi0312@gmail.com>
2020-09-21 14:05:55 +0530

    autos

--- flaskapp.py
+++ flaskapp.py
@@ -11,7 +11,7 @@ from flask import Flask, render_template
 
 
 app = Flask(__name__)
-#utos = get_autos("YearbookENTC", download_image=False)
+#autos = get_autos("YearbookENTC", download_image=False)
 
 autos = get_autos("YearbookENTC", download_image=False)
 
